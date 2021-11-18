ab22ad827aaed0c9edf353ae930cf19fdeeb1192
Jeferson <Jeferson.fernandesd5@gmail.com>
2021-11-18 14:07:59 -0300

    atualização

--- javascript/produtos.js
+++ javascript/produtos.js
@@ -1,28 +1,28 @@
 const produtos = [
   {
     id: 1,
-    image:'fD:\BKP DADOS\Documents\whatsapprestaurante-main\imagens',
+    image:'file:///D:/BKP%20DADOS/Videos/Teste1/imagens/avante0.1.png',
     title: 'avanteviajante 0.1',
     description: 'Descricão do produto 01',
     value: 10.00
   },
   {
     id: 2,
-    image: 'https://picsum.photos/200',
+    image: 'file:///D:/BKP%20DADOS/Videos/Teste1/imagens/avante0.1.png',
     title: 'Produto 02',
     description: 'Descricão do produto 02',
     value: 20.00
   },
   {
     id: 3,
-    image: 'https://picsum.photos/200',
+    image: 'file:///D:/BKP%20DADOS/Videos/Teste1/imagens/avante0.1.png',
     title: 'Produto 03',
     description: 'Descricão do produto 03',
     value: 30.00
   },
   {
     id: 4,
-    image: 'https://picsum.photos/200',
+    image: 'file:///D:/BKP%20DADOS/Videos/Teste1/imagens/avante0.1.png',
     title: 'Produto 04',
     description: 'Descricão do produto 04',
     value: 40.00
